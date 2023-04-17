0801338987b3d88f84e45a39a08cbf70dc0d74c0
RTaylorAshka <121143279+RTaylorAshka@users.noreply.github.com>
2023-04-17 13:31:59 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -12,3 +12,61 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
+@app.route('/')
+def red_home():
+    return redirect('/users')
+
+@app.route('/users')
+def get_users():
+    users = User.query.all()
+    return render_template('user-list.html', users = users)
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user-page.html', user = user)
+
+@app.route('/users/new')
+def new_user_form():
+    return render_template('user-form.html')
+
+@app.route('/users/new', methods = ['POST'])
+def save_new_user():
+    fn = request.form["first_name"]
+    ln = request.form["last_name"]
+    img = request.form["image_url"]
+
+    new_user = User(first_name = fn, last_name = ln, image_url = img)
+    with app.app_context():
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user-edit', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods = ['POST'])
+def edit_user(user_id):
+    fn = request.form["first_name"]
+    ln = request.form["last_name"]
+    img = request.form["image_url"]
+
+    user = User.query.get(user_id)
+    user.first_name = fn
+    user.last_name = ln
+    user.image_url = img
+
+    with app.app_context():
+        db.session.add(user)
+        db.session.commit()
+
+
+
+# with app.app_context():
+#     db.create_all()
+# with app.app_context():
+#     db.session.add(timmy)
+#     db.session.commit()
--- models.py
+++ models.py
@@ -6,12 +6,18 @@ db = SQLAlchemy()
 
 def connect_db(app):
     
+    
     db.app = app
     db.init_app(app)
 
 
 class User(db.Model):
+    
     __tablename__ = 'users'
+
+    def __repr__(self):
+        u = self
+        return f"<User_id = {u.id}, first_name = {u.first_name}, last_name = {u.last_name}, img_url = {u.image_url}>"
     id = db.Column(db.Integer,
                    primary_key = True,
                    autoincrement = True)
@@ -22,5 +28,8 @@ class User(db.Model):
                            nullable = False,
                            )
     image_url = db.Column(db.String,
-                           nullable = False,
+                           default = 'https://imgs.search.brave.com/j5hY6z-xlTQo6Y9Il0aC36Y7dT7apapohmImQVjul8Y/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzE2LzFh/L2NmLzE2MWFjZmJl/MDQyMGQxNjc2ZGFi/ZjQ1OTljYWViZDMy/LmpwZw'
                            )
+    
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
