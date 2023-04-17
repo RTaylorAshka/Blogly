82477edbda84625e4945bce0f47d8144ee73756e
Halim Tannous <48407395+halimt92@users.noreply.github.com>
2023-04-17 22:41:08 +0200

    post session

--- app.py
+++ app.py
@@ -9,47 +9,54 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
-
+app.app_context().push()
 connect_db(app)
+db.create_all()
+
 
 @app.route('/')
 def red_home():
     return redirect('/users')
 
+
 @app.route('/users')
 def get_users():
     users = User.query.all()
-    return render_template('user-list.html', users = users)
+    return render_template('user-list.html', users=users)
+
 
 @app.route('/users/<int:user_id>')
 def show_user(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template('user-page.html', user = user)
+    return render_template('user-page.html', user=user)
+
 
 @app.route('/users/new')
 def new_user_form():
     return render_template('user-form.html')
 
-@app.route('/users/new', methods = ['POST'])
+
+@app.route('/users/new', methods=['POST'])
 def save_new_user():
     fn = request.form["first_name"]
     ln = request.form["last_name"]
     img = request.form["image_url"]
 
-    new_user = User(first_name = fn, last_name = ln, image_url = img)
-    with app.app_context():
-        db.session.add(new_user)
-        db.session.commit()
+    new_user = User(first_name=fn, last_name=ln, image_url=img)
+    db.session.add(new_user)
+    db.session.commit()
 
     return redirect('/users')
 
-@app.route('/users/<int:user_id>/edit')
-def edit_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return render_template('user-edit', user=user)
 
-@app.route('/users/<int:user_id>/edit', methods = ['POST'])
-def edit_user(user_id):
+@app.route('/users/<int:user_id>/edit')
+def edit_user_interface(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user-edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def edit_user_action(user_id):
     fn = request.form["first_name"]
     ln = request.form["last_name"]
     img = request.form["image_url"]
@@ -58,11 +65,9 @@ def edit_user(user_id):
     user.first_name = fn
     user.last_name = ln
     user.image_url = img
-
-    with app.app_context():
-        db.session.add(user)
-        db.session.commit()
-
+    db.session.add(user)
+    db.session.commit()
+    return redirect(f'/users/{user_id}')
 
 
 # with app.app_context():
--- models.py
+++ models.py
@@ -4,32 +4,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
-    
-    
     db.app = app
     db.init_app(app)
 
 
 class User(db.Model):
-    
+
     __tablename__ = 'users'
 
     def __repr__(self):
         u = self
         return f"<User_id = {u.id}, first_name = {u.first_name}, last_name = {u.last_name}, img_url = {u.image_url}>"
     id = db.Column(db.Integer,
-                   primary_key = True,
-                   autoincrement = True)
+                   primary_key=True,
+                   autoincrement=True)
     first_name = db.Column(db.String,
-                           nullable = False,
+                           nullable=False,
                            )
     last_name = db.Column(db.String,
-                           nullable = False,
-                           )
+                          nullable=False,
+                          )
     image_url = db.Column(db.String,
-                           default = 'https://imgs.search.brave.com/j5hY6z-xlTQo6Y9Il0aC36Y7dT7apapohmImQVjul8Y/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzE2LzFh/L2NmLzE2MWFjZmJl/MDQyMGQxNjc2ZGFi/ZjQ1OTljYWViZDMy/LmpwZw'
-                           )
-    
+                          default='https://imgs.search.brave.com/j5hY6z-xlTQo6Y9Il0aC36Y7dT7apapohmImQVjul8Y/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzE2LzFh/L2NmLzE2MWFjZmJl/MDQyMGQxNjc2ZGFi/ZjQ1OTljYWViZDMy/LmpwZw'
+                          )
+
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
