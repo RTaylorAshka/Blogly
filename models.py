c893eb3519c6f6722f28114d178d5946e1bc3fb5
RTaylorAshka <121143279+RTaylorAshka@users.noreply.github.com>
2023-04-21 04:01:04 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -33,3 +34,28 @@ class User(db.Model):
     
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+
+class Post(db.Model):
+
+    __tablename__ = 'posts'
+
+    def __repr__(self):
+        p = self
+        return f"{p}"
+    
+    id = db.Column(db.Integer,
+                   primary_key = True,
+                   autoincrement = True)
+    title = db.Column(db.String,
+                      nullable = False
+                      )
+    content = db.Column(db.String,
+                        nullable = False)
+    created_at = db.Column(db.DateTime,
+                           default = datetime.now()
+                           )
+    user_id = db.Column(db.Integer, 
+                        db.ForeignKey('users.id')
+                        )
+    
---
+++ seed.py
@@ -0,0 +1,49 @@
+from models import db, User, Post
+from app import app
+
+
+db.drop_all()
+db.create_all()
+
+
+u1 = User(first_name = 'Tim', last_name = 'Iwdkal')
+u2 = User(first_name = 'Dan', last_name = 'Ipsum')
+u3 = User(first_name = 'Amy', last_name = 'Doaralks')
+u4 = User(first_name = 'Jimmy', last_name = 'Alwdi')
+u5 = User(first_name = 'Timmy', last_name = 'Oakwod')
+
+db.session.add(u1)
+db.session.add(u2)
+db.session.add(u3)
+db.session.add(u4)
+db.session.add(u5)
+
+p1 = Post(title = 'Lorem Ipsum', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 1)
+
+p2 = Post(title = 'Lorem Twopsum', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 2)
+p3 = Post(title = 'Threelom Twopsum', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 2)
+p4 = Post(title = 'Fourlum Ipsum', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 2)
+p5 = Post(title = 'Loreeeem Ipsuuum', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 3)
+p6 = Post(title = 'lOrEM iPsuM', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 4)
+p7 = Post(title = 'Lorem', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 4)
+p8 = Post(title = 'IpsummMMmm', content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+          user_id = 5)
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.add(p3)
+db.session.add(p4)
+db.session.add(p5)
+db.session.add(p6)
+db.session.add(p7)
+db.session.add(p8)
+
+
+db.session.commit()
